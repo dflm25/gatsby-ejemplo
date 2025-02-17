0a150ff52bbc997768611aeb543e1b942fd9dbd5
Lucumi <dlucumi@coredigital.com>
2025-02-17 12:05:10 -0500

    nueva historia

---
+++ src/pages/dias-grises-de-un-baile-inolvidable.jsx
@@ -0,0 +1,254 @@
+import * as React from "react";
+import ConfettiExplosion from "react-confetti-explosion";
+
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Logo from "../assets/avlogo.webp";
+import Sandwich from "../assets/sandwich.webp";
+import Hamburguer from "../assets/hamburguer.webp";
+import salento from "../assets/salento.jpg";
+
+const IndexPage = () => {
+    return (
+        <>
+            <Header showBio={false} />
+            <section id="projects" class="project-container container">
+                <div class="division"></div>
+                <div class="content-text">
+                    <h2>Historia no contada</h2>
+                    <p>Dias grises de un baile inolvidable!</p>
+                </div>
+                <iframe src="https://open.spotify.com/embed/track/2lTm559tuIvatlT1u0JYG2?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <br />
+                <article class="blog-content">
+                    <p>
+                        Días grises llegan a esta historia de amor. Entre más amor hay, más necesidad surge de saber del otro, de ser parte de la vida del otro.
+                        Algunos sienten esa necesidad más que otros. Esta intensa conexión, sumada a la falta de comunicación, ha comenzado a generar malentendidos entre
+                        Alejandro y Daniela. Lo que antes era una relación llena de pasión y ternura, ahora se ve amenazada por silencios y suposiciones.
+                    </p>
+                    <br />
+                    <p>
+                        Todo comenzó cuando Alejandro, después de una semana extremadamente pesada de trabajo y con los pendientes de un hogar que estaba construyendo,
+                        llegó al fin de semana. Lo único que deseaba era estar con Daniela, su refugio, su desconexión de un mundo irreal de responsabilidades adultas,
+                        para sumergirse en días de dar y recibir amor.
+                    </p>
+                    <br />
+                    <p>
+                        Sin embargo, no siempre las cosas salen como se esperan. Daniela también venía de una semana dura, lidiando con sus propios asuntos, y
+                        anhelaba que el fin de semana fuera perfecto para ambos. Pero al leer el mensaje de Alejandro, su alegría se transformó en tristeza.
+                        Un pensamiento recurrente invadió su mente: "Siempre es lo mismo con Alejandro. Cuando quiero que todo sea perfecto, algo parece salir mal".
+                        Daniela tenía mil ideas y planes para hacer de ese fin de semana algo especial, para atender y consentir al hombre que amaba.
+                    </p>
+                    <br />
+                    <p>
+                        Alejandro, con el peso de la semana, le expresó a Daniela lo cansado que estaba. Ella, que esperaba celebrar su primer mes de noviazgo,
+                        cambió su ánimo de inmediato. No quería que Alejandro cambiara sus planes, pero tampoco queria hacer algo que no disfrutaría.
+                    </p>
+                    <br />
+                    <p>
+                        "Daniela", estoy cansado, pero quiero salir contigo expreso Alejandro, con la esperanza de que ella entendiera su situación.
+                        Él también tenía planes para ese fin de semana; quería estar con ella, compartir momentos especiales y desconectarse del estrés diario,
+                        pero Daniela interpreto las palabras de Alejandro como falta de interes.
+                    </p>
+                    <br />
+                    <p>
+                        <strong>Si estás cansado, entonces ve a dormir, </strong> Alejandro, se toma todo literal al pie de la letra,
+                        decidió hacer justamente eso. Pero antes de ir a dormir, recordo que no habia comido nada en todo el día,
+                        esperando pasar la noche con Daniela en un lugar agradable,
+                        disfrutando de una cena deliciosa. Así que, con el estómago vacío y el corazon triste, salió a buscar algo de comer,
+                        pensando en cómo las cosas no eran como se soñaban.
+                    </p>
+                    <br />
+                    <p>
+                        Alejandro llegó al sitio de comida y, mientras esperaba su pedido, notó varias llamadas perdidas de Daniela. Preocupado, decidió devolverle la llamada.
+                    </p>
+                    <br />
+                    <p>
+                        <strong>—¿Dónde estás? ¿No piensas venir?</strong> —preguntó Daniela, con un tono de voz que mezclaba preocupación y frustración.
+                        me mandaste a dormir. ¿Cómo iba a saber que me estabas esperando después de eso? —respondió Alejandro, confundido y frustrado por querer estar alla con ella.
+                    </p>
+                    <br />
+                    <p>
+                        Sin embargo, la ira y la confusión se apoderaron de ambos. Alejandro, frustrado, expresó estar cansado de recibir críticas
+                        constantes por parte de Daniela. Ella, a su vez, reveló su frustración por no poder tener una relación normal,
+                        sintiendo que Alejandro no entendía sus necesidades.
+                    </p>
+                    <br />
+                    <p class="dialogo">
+                        <strong>¡Estoy cansada de esto!</strong>, Alejandro sintio las lágrimas de Daniela. No puedo seguir así,
+                        siempre sintiendo que no soy importante para ti.
+                    </p>
+                    <br />
+                    <p>
+                        Alejandro, herido por sus palabras, respondió: crees que para mí es fácil? Siempre estoy tratando de hacer lo correcto,
+                        pero parece que nunca es suficiente.
+                    </p>
+                    <br />
+                    <p>La tensión llegó a un punto crítico. Daniela, con la voz temblorosa, tomó una decisión drástica: Definitivamente, no quiero estar con alguien así. Esto se acaba aquí.</p>
+                    <br />
+                    <p>Alejandro solo dijo estas segura cuando realmente lo que queria decir es ven Daniela te amo quiero verte espera por mi pero el orgullo gano a las palabras de Daniela.</p>
+                    <br />
+                    <p>
+                        Daniela lo bloqueó de su vida. Para él, fue un golpe. Se sintió como lo peor del mundo, sumido en una tristeza profunda.
+                        Aunque en ese momento creyó que no podía sentirse peor, pronto se daría cuenta de que ese dolor no era nada comparado con lo que le esperaba en el futuro.
+                    </p>
+                    <br />
+                    <p>
+                        Pasaron los días, y aunque el dolor seguía presente, la distancia permitió que ambos reflexionaran sobre lo sucedido. Daniela, después de meditarlo,
+                        decidió romper el silencio y hablar con Alejandro. Le envió un mensaje para comunicarle que quería entregarle algunas cosas que aún tenía de él.
+                    </p>
+                    <br />
+                    <p>
+                        Alejandro, con una mezcla de esperanza y nerviosismo, respondió al mensaje. La posibilidad de volver a hablar con Daniela, aunque fuera por un motivo práctico,
+                        le dio esperanza. Poco a poco, comenzaron a intercambiar mensajes, y pronto las conversaciones se volvieron más fluidas.
+                        El amor que sentían el uno por el otro era demasiado fuerte para ignorarlo.
+                    </p>
+                    <br />
+                    <p>
+                        Después de ese intercambio de mensajes, se dio la oportunidad de hablar cara a cara. En la mente de Alejandro había mucha confusión,
+                        pero una cosa siempre había estado clara: su amor por Daniela. Se vieron un día, y Alejandro, con el corazón abierto, expresó todos sus sentimientos
+                        y deseos. Daniela lo escuchó atentamente, sin decir mucho, pero sus ojos reflejaban una mezcla de emociones.
+                    </p>
+                    <br />
+                    <p>
+                        Daniela, aunque le costaba expresar sus sentimientos con palabras, dejó que sus acciones hablaran por ella. Ese día, hicieron el amor,
+                        y Alejandro sintió que todo era diferente. Le encantaba mirarla a los ojos, ver su rostro y esa ternura que solo Daniela podía transmitir.
+                        En esos momentos, su mundo cambiaba por completo.
+                    </p>
+                    <br />
+                    <p>
+                        Al llegar a casa Alejandro sintio que habia sido una noche increible por estar con la mujer que amaba pero siempre sintio ese mal sabor de no tener la claridad
+                        de lo que realmente pasaba entre ellos. La falta de comunicación seguía siendo un obstáculo, y aunque ambos estaban dispuestos a trabajar en ello,
+                        había un vacío que necesitaba ser llenado. Alejandro no quería que su relación se convirtiera en una montaña rusa de emociones,
+                        y sabía que era fundamental abordar los problemas de fondo pero no lo expreso asi solo dejo pasar el tiempo.
+                    </p>
+                    <br />
+                    <p>
+                        Un día, mientras Daniela trabajaba desde casa, tuvo un incidente. Alejandro, al enterarse, se preocupó inmediatamente por ella y sintió
+                        que necesitaba de él. Sin dudarlo, fue a su casa, le preparó comida y cuidó su tobillo lastimado. En ese momento,
+                        Alejandro se sintió como el protagonista de una comedia romántica, listo para levantar a su amada en brazos y decirle:
+                    </p>
+                    <br />
+                    <p>
+                        <strong>—No te preocupes</strong>, aquí me tienes. Soy todo lo que necesitas. Sabes que puedes contar conmigo.
+                        <br />
+                        <strong> —pero el es un idiota que en su confusion no dice las cosas que siente.</strong>
+                    </p>
+                    <br />
+                    <p>
+                        Ese día fue especial. Daniela, aunque guardaba silencio, tenía una expresión que decía más que mil palabras.
+                        Sus ojos brillaban con gratitud y amor,
+                        y Alejandro podía ver en su rostro que ella quería decirle muchas cosas, pero las palabras parecían no ser suficientes.
+                        <br />
+                        <strong> —pero el es un idiota que en su confusion no pregunta que quieres.</strong>
+                    </p>
+                    <p>
+                        Ese día, se abrazaron y se acariciaron, expresando ese amor que sentían el uno por el otro. Para Alejandro,
+                        no había mejor refugio que los brazos de Daniela, quien, sin necesidad de palabras, le transmitía todo su amor.
+                    </p>
+                    <br />
+                    <p>
+                        En su mente, Alejandro sabía que no estaba actuando de la manera más racional, pero una vez más,
+                        se dejó llevar por la pasión en lugar de la razón. Hicieron el amor, y Alejandro se sintió único al escuchar
+                        los gemidos de su amada. Sabía que ese era el lugar donde siempre había querido estar.
+                    </p>
+                    <br />
+                    <p>
+                        La noche avanzaba, y Alejandro comenzó a preocuparse por la hora y su semana laboral. Pensó en marcharse,
+                        pero justo en ese momento, Daniela lo detuvo. Con lágrimas en los ojos, le expresó:
+                    </p>
+                    <br />
+                    <p>
+                        <strong>—Alejandro, te amo y quiero estar contigo. </strong> Alejandro se quedó en shock. En medio de toda esa confusión,
+                        lo único que se le ocurrió decir fue: <strong> —Tengo que pensarlo.</strong>
+                    </p>
+                    <br />
+                    <p>Aunque en su corazón la respuesta siempre había sido un sí rotundo, las palabras no salieron como él hubiera querido.</p>
+                    <p>
+                        Ese día, Alejandro salió de la casa de Daniela con mil pensamientos en su cabeza. Se sentía feliz de estar con el amor de su vida,
+                        pero también triste por las situaciones que habían atravesado. La incertidumbre y la confusión lo acompañaron mientras se alejaba,
+                        dejando atrás un momento que podría haber sido perfecto. solo con decir si!
+                    </p>
+                    <br />
+                    <p>
+                        Pasaron los días, y Alejandro sentía un nudo en la garganta. Sabía que las cosas no estaban bien, así que decidió hablar con Daniela.
+                        Pensó que, después de todo lo que habían pasado, el amor que sentían el uno por el otro era lo suficientemente fuerte
+                        como para encontrar una solución. Con entusiasmo, le pidió a Daniela que se vieran, convencido de que podrían llegar a un
+                        acuerdo sobre cómo deberían ser las cosas entre ellos.
+                    </p>
+                    <br />
+                    <p>
+                        Sin embargo, Alejandro no imaginaba que esa conversación sería el inicio del fin. Daniela, que había estado reflexionando
+                        mucho durante esos días, ya tenía otras cosas en su corazón. Cuando se encontraron, la tensión en el aire era palpable.
+                    </p>
+                    <br />
+                    <p>
+                        —Daniela, sé que hemos pasado por mucho, pero creo que podemos encontrar una manera de hacer que esto funcione
+                        —dijo Alejandro, con esperanza en su voz.
+                    </p>
+                    <br />
+                    <p>
+                        Daniela lo miró con tristeza y determinación en sus ojos. y le dijo: <strong>no quiero hablar solo tengo hambre.</strong> para Alejandro fue agua fria que cayo sobre su cabeza.
+                    </p>
+                    <br />
+                    <p>Se dirigieron al coche. Daniela le pidió a Alejandro que manejara. Él se subió y, cuando fue a encender el motor, Daniela lo detuvo.</p>
+                    <br />
+                    <p>—No arranques. Quiero hablar —dijo con voz temblorosa.</p>
+                    <br />
+                    <p>En ese momento, Alejandro supo que nada volvería a ser igual.</p>
+                    <br />
+                    <p>
+                        Daniela estalló en llanto y, con palabras cargadas de dolor y resentimiento, le expresó a Alejandro todo lo que había estado guardando.
+                        Le enumeró uno a uno sus errores, con una intensidad que él nunca había visto en ella.
+                    </p>
+                    <br />
+                    <p>—Has sido lo peor que me ha pasado. Me siento estúpida por haber aceptado a alguien con tantos errores y tan poco corazón.</p>
+                    <br />
+                    <p>
+                        Alejandro, con lágrimas en los ojos, no pudo hacer más que aceptar sus palabras. Daniela no quería escuchar nada de lo que él
+                        tuviera que decir. Le pidió que la llevara a su casa y, al llegar, le ordenó que se bajara del coche.
+                    </p>
+                    <br />
+                    <p>
+                        —No me hables nunca más —dijo con firmeza—. Eres lo peor que me ha pasado en la vida.
+                    </p>
+                    <p>
+                        Alejandro, desesperado, le pidió a Daniela que se acercara. Solo quería ver en sus ojos lo que sus palabras decían.
+                        Al acercarse, vio una mirada de decepción y dolor, pero también un destello de amor que aún persistía.
+                    </p>
+                    <br />
+                    <p><strong>DANIELA YO A TI TE AMO</strong>. Aunque tú no lo quieras, con el corazón destrozado.</p>
+                    <br />
+                    <p>
+                        Triste y derrotado, Alejandro se retiró, sabiendo que los días que vendrían serían insoportables. La vida sin ella no tenía sentido.
+                        No había motivación para hacer las cosas bien, ni tranquilidad para dormir. Daniela había sido una parte fundamental de su vida,
+                        y ahora se sentía perdido sin ella.
+                    </p>
+                    <p>Todas las historias tienen un fin, y este no fue el adecuado. Cuando el amor es tan fuerte e intenso, vale la pena luchar por él.</p>
+                    <br />
+                    <p>Ambos cometieron errores, pero las palabras, las confusiones y el orgullo de ambos arrojaron todo por un abismo.</p>
+                    <br />
+                    <p>
+                        Hoy en día, Alejandro se siente profundamente triste y recuerda con mucho amor a la mujer perfecta para él,
+                        aquella que le brindó un amor tan bonito. La llevará siempre en su corazón y deseará eternamente que esté muy bien.
+                    </p>
+                    <br />
+                    <p>Sin embargo, el dolor siempre estará presente, porque los lugares que se volvieron lindos y perfectos recuerdos ya no serán lo mismo sin ella.</p>
+                    <br />
+                    <div className="text-center">
+                        <img src={salento} alt="Salento sin ella" className="salento" />
+                    </div>
+                    <br />
+                    <div className="text-center">
+                        <h2>TE AMO ALE </h2> 🥹
+                    </div>
+                </article>
+            </section>
+            <Footer />
+        </>
+    );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Daniel Lucumi | Software Engineer</title>;
